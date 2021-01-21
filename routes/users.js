72649f111fc038aed9f59056f2c6af9a11d4d5ea
Hamza <hamza.ishak.ali@gmail.com>
2021-01-21 19:39:44 +0800

    Refactor

--- routes/users.js
+++ routes/users.js
@@ -10,25 +10,17 @@ router.post('/', create)
 router.delete('/:id', deleteOne)
 
 function getAll(request, response) {
-    console.log('>> ', request.user)
-
     User.getAll()
-        .then(users => {    
-            response.json(users)
-        })
+        .then(users => response.json(users))
         .catch(error => {
-            console.log(error)
             response.status(403).json({ error: `Unable to fetch users. ${error.message}` })
         })
 }
 
 function getOne(request, response) {
     User.get(request.params.id)
-        .then(user => {
-            response.json(user)
-        })
+        .then(user => response.json(user))
         .catch(error => {
-            console.log(error)
             response.status(403).json({ error: `Unable to fetch user. ${error.message}` })
         })
 }
@@ -37,11 +29,8 @@ function getOne(request, response) {
 function create(request, response) {
     const { name, username, email, password, role } = request.body;
     User.create(name, username, email, password, role)
-        .then(() => {
-            response.status(200).send('User successfully created!')
-        })
+        .then(() => response.status(200).send('User successfully created!'))
         .catch(error => {
-            console.log(error)
             response.status(400).json({ error: `Failed to create user. ${error.message} `})
         })
 }
@@ -57,11 +46,8 @@ function deleteOne(request, response) {
     }
 
     User.delete(id)
-        .then(() => {
-            response.send('User deleted successfully!')
-        })
+        .then(() => response.send('User deleted successfully!'))
         .catch(error => {
-            console.log(error)
             response.status(403).json({ error: `Failed to delete user. ${error.message}` })
         })
 }
